ad4841df60d80a86c170860fd2fdeb6780af943f
axle07 <joeblack78006@yahoo.com>
2015-05-22 16:02:03 -0700

    v2.1

--- color_picker.rb
+++ color_picker.rb
@@ -1,6 +1,5 @@
 $colors = Array.new
-#intervals = [128, 64, 192, 32, 96, 160, 224]
-intervals = [224, 32, 128, 160, 96, 192, 64].reverse!
+intervals = [255, 0, 128, 64, 192, 32, 96, 160, 224]
 
 def generate(n)
   a = [n, 255, 0]
@@ -8,8 +7,8 @@ def generate(n)
 end
 
 intervals.map { |i| generate(i) }
-$colors.flatten!(1)
+$colors.flatten!(1).uniq!
 
 puts "<html><head></head><body>"
-$colors.map { |c| puts '<div style="height: 100px; width: 100%; background-color: rgb(' + c[0].to_s + ',' +  c[1].to_s + ',' +  c[2].to_s + ')"></div>' }
+$colors.map { |c| puts '<div style="height: 50px; width: 100%; background-color: rgb(' + c[0].to_s + ',' +  c[1].to_s + ',' +  c[2].to_s + ')"></div>' }
 puts "</body></html>"
